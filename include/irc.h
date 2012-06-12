a49a1b0bf1bc9d29e8e8e841128eb545b2cd92b6
Manuel Heim <manuel.heim@fh-heidelberg.de>
2012-06-12 11:10:30 +0200

    header file for irc operations

---
+++ include/irc.h
@@ -0,0 +1,18 @@
+#ifndef IRC_H
+#define IRC_H
+
+typedef struct
+{
+	char* channel;
+	char* nick;
+} irc_ctx_t;
+
+int connect(irc_callbacks_t* callbacks, irc_ctx_t* ctx, irc_session_t* session, char* server, char* nick, char* channel);
+
+void event_connect(irc_session_t* session, const char* event, const char* origin, const char** params, unsigned int count);
+
+void event_join(irc_session_t* session, const char* event, const char* origin, const char** params, unsigned int count);
+
+void event_channel(irc_session_t* session, const char* event, const char* origin, const char** params, unsigned int count);
+
+#endif
